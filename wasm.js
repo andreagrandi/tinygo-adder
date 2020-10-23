773eeae7d2e08691aa2841439eef4ebf05c2a55e
Andrea Grandi <a.grandi@gmail.com>
2020-10-23 17:29:53 +0200

    Fix example

--- wasm.js
+++ wasm.js
@@ -5,22 +5,12 @@ var wasm;
 
 function init() {
     const go = new Go();
+
     if ('instantiateStreaming' in WebAssembly) {
         WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
             wasm = obj.instance;
             go.run(wasm);
-            updateResult();
         })
-    } else {
-        fetch(WASM_URL).then(resp =>
-            resp.arrayBuffer()
-        ).then(bytes =>
-            WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
-                wasm = obj.instance;
-                go.run(wasm);
-                updateResult();
-            })
-        )
     }
 }
 
